08932e39287a52675d8b7449f34acc2e2928bf37
wheatup <acphun@gmail.com>
2019-04-26 11:41:29 +0900

    transformation done

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 const htmlTester = /^<.*>$/g;
 
 class Doper {
-	constructor(dom, parent) {
+	constructor(dom, parent, params, css) {
 		this.dom = dom;
 		this.parent = parent;
-		this.x = 0;
-		this.y = 0;
-		this.scale = 1;
-		this.rotation = 0;
+
+		this._x = 0;
+		this._y = 0;
+		this._localX = 0;
+		this._localY = 0;
+		this._y = 0;
+		this._width = 100;
+		this._height = 100;
+		this._scale = 1;
+		this._scaleX = 1;
+		this._scaleY = 1;
+		this._pivotX = 0.5;
+		this._pivotY = 0.5;
+		this._minAnchorX = 0;
+		this._maxAnchorX = 1;
+		this._minAnchorY = 0;
+		this._maxAnchorY = 1;
+		this._rotation = 0;
 		this.active = true;
+
+		if (params) {
+			Object.assign(this, params);
+		}
+
+		Object.assign(this._dom.style, { position: 'absolute' });
+
+		if (css) {
+			Object.assign(this._dom.style, css);
+		}
+
 		if (this._parent) {
 			this._parent.appendChild(this.dom);
 		}
+
+		this._update();
+
+		window.addEventListener('resize', this._update.bind(this));
 	}
 
 	destroy() {
@@ -20,9 +49,86 @@ class Doper {
 	}
 
 	_update() {
-		if(!this.active) return;
-		let transform = `translate3D(${this.x}px, ${this.y}px, 0px) scale(${this.scaleX}, ${this.scaleY}) rotate(${this.rotation}deg)`;
-		Object.assign(this.dom.style, { transform });
+		if (!this.active) return;
+		let transformOrigin = `${100 * this._pivotX}% ${100 * this._pivotY}%`;
+		let transform = `scale(${this._scaleX * this._scale}, ${this._scaleY * this._scale}) rotate(${this._rotation}deg)`;
+
+		if (!isNaN(this._left) && !isNaN(this._right)) {
+			this.__left = this._parent.clientWidth * this._minAnchorX + this._left + this._x;
+			this.__right = this._parent.clientWidth * (1 - this._maxAnchorX) + this._right - this._x;
+		} else if (!isNaN(this._left) && isNaN(this._right)) {
+			this.__left = this._parent.clientWidth * this._minAnchorX + this._left + this._x;
+			this.__right = this._parent.clientWidth - (this.__left + this._width);
+		} else if (isNaN(this._left) && !isNaN(this._right)) {
+			this.__right = this._parent.clientWidth * (1 - this._maxAnchorX) + this._right - this._x;
+			this.__left = this._parent.clientWidth - this.__right - this._width;
+		} else {
+			this.__left = this._minAnchorX * this._parent.clientWidth - this._width * this._pivotX + this._x;
+			this.__right = this._parent.clientWidth * (1 - this._maxAnchorX) - this._width * (1 - this._pivotX) + this._x;
+		}
+
+		if (!isNaN(this._top) && !isNaN(this._bottom)) {
+			this.__top = this._parent.clientHeight * this._minAnchorY + this._top + this._y;
+			this.__bottom = this._parent.clientHeight * (1 - this._maxAnchorY) + this._bottom - this._y;
+		} else if (!isNaN(this._top) && isNaN(this._bottom)) {
+			this.__top = this._parent.clientHeight * this._minAnchorY + this._top + this._y;
+			this.__bottom = this._parent.clientHeight - (this.__top + this._height);
+		} else if (isNaN(this._top) && !isNaN(this._bottom)) {
+			this.__bottom = this._parent.clientHeight * (1 - this._maxAnchorY) + this._bottom - this._y;
+			this.__top = this._parent.clientHeight - this.__bottom - this._height;
+		} else {
+			this.__top = this._minAnchorY * this._parent.clientHeight - this._height * this._pivotY + this._y;
+			this.__bottom = this._parent.clientHeight * (1 - this._maxAnchorY) - this._height * (1 - this._pivotY) + this._y;
+		}
+
+		let left = this.__left + 'px';
+		let right = this.__right + 'px';
+		let top = this.__top + 'px';
+		let bottom = this.__bottom + 'px';
+		console.log(transformOrigin);
+		Object.assign(this.dom.style, { transformOrigin, transform, left, right, top, bottom });
+
+		// let left = this._minAnchorX * this._parent.clientWidth - this._width * this._pivotX + this._x + 'px';
+		// let top = this._minAnchorY * this._parent.clientHeight - this._height * this._pivotY + this._y + 'px';
+		// let width = this._width + 'px';
+		// let height = this._height + 'px';
+		// Object.assign(this.dom.style, { transformOrigin, transform, left, top, width, height });
+	}
+
+	get top() {
+		return this._top;
+	}
+
+	set top(value) {
+		this._top = value;
+		this._update();
+	}
+
+	get bottom() {
+		return this._bottom;
+	}
+
+	set bottom(value) {
+		this._bottom = value;
+		this._update();
+	}
+
+	get left() {
+		return this._left;
+	}
+
+	set left(value) {
+		this._left = value;
+		this._update();
+	}
+
+	get right() {
+		return this._right;
+	}
+
+	set right(value) {
+		this._right = value;
+		this._update();
 	}
 
 	get style() {
@@ -35,6 +141,78 @@ class Doper {
 
 	set rotation(value) {
 		this._rotation = value;
+		this._update();
+	}
+
+	get height() {
+		return this._height;
+	}
+
+	set height(value) {
+		this._height = value;
+		this._update();
+	}
+
+	get width() {
+		return this._width;
+	}
+
+	set width(value) {
+		this._width = value;
+		this._update();
+	}
+
+	get maxAnchorY() {
+		return this._maxAnchorY;
+	}
+
+	set maxAnchorY(value) {
+		this._maxAnchorY = value;
+		this._update();
+	}
+
+	get minAnchorY() {
+		return this._minAnchorY;
+	}
+
+	set minAnchorY(value) {
+		this._minAnchorY = value;
+		this._update();
+	}
+
+	get minAnchorX() {
+		return this._minAnchorX;
+	}
+
+	set minAnchorX(value) {
+		this._minAnchorX = value;
+		this._update();
+	}
+
+	get maxAnchorX() {
+		return this._maxAnchorX;
+	}
+
+	set maxAnchorX(value) {
+		this._maxAnchorX = value;
+		this._update();
+	}
+
+	get pivotX() {
+		return this._pivotX;
+	}
+
+	set pivotX(value) {
+		this._pivotX = value;
+		this._update();
+	}
+
+	get pivotY() {
+		return this._pivotY;
+	}
+
+	set pivotY(value) {
+		this._pivotY = value;
 		this._update();
 	}
 
@@ -66,6 +244,25 @@ class Doper {
 		this._scaleX = value;
 		this._update();
 	}
+
+	get localY() {
+		return this._localY;
+	}
+
+	set localY(value) {
+		this._localY = value;
+		this._update();
+	}
+
+	get localX() {
+		return this._localX;
+	}
+
+	set localX(value) {
+		this._localX = value;
+		this._update();
+	}
+
 
 	get y() {
 		return this._y;
@@ -141,11 +338,13 @@ function getDom(dom) {
 	return _dom;
 }
 
-const doper = function (dom, parent) {
+const doper = function (dom, parent, params, css) {
 	if (!dom) return;
 	let _dom = getDom(dom);
 	let _parent = getDom(parent);
-	return new Doper(_dom, _parent);
+	if (_dom) {
+		return new Doper(_dom, _parent, params, css);
+	}
 }
 
 if (typeof module === 'object') {
--- test/test.js
+++ test/test.js
@@ -1,22 +1,37 @@
 window.onload = function () {
-	let node = doper('<div>Hello World!</div>', '.container');
+	let node = doper(
+		'<div>Hello World!</div>',
+		'.container',
+		{
+			x: 0,
+			y: 0,
+			width: 100,
+			pivotX: 0.5,
+			pivotY: 0.5,
+			left: 120,
+			right: 200,
+			top: 20,
+			bottom: 20,
+			minAnchorX: 0,
+			maxAnchorX: 1,
+			minAnchorY: 0,
+			maxAnchorY: 1,
+			pivotX: 0.5,
+			pivotY: 0.5,
+			rotation: 50
+		}, {
+			backgroundColor: 'salmon',
+		});
+
 	Object.assign(node.parent.style, {
-		display: 'flex',
-		justifyContent: 'center',
-		alignItems: 'center',
 		position: 'relative'
 	});
-	node.style.float = 'left';
-	node.style.position = 'absolute';
 
-	let tick = 0;
 	function update(){
-		tick++;
-		node.x = Math.cos(tick * 0.05) * 200;
-		node.y = Math.sin(tick * 0.05) * 200;
-		node.scale = (1 + Math.cos(tick * 0.075) * 0.5) * 2;
-		node.rotation = tick * 2;
+		node.rotation++;
+		node.right-= 1;
 		requestAnimationFrame(update);
 	}
+
 	update();
 };
